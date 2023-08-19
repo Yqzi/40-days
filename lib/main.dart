4e276f7848c8f5eefef732b013bdd13aa6c34618
Yqzi <y.qazi12@gmail.com>
2023-08-19 14:39:18 -0700

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -90,58 +90,76 @@ class CustomAlertDialog extends StatefulWidget {
 
 class _CustomAlertDialogState extends State<CustomAlertDialog> {
   TextEditingController taskNameController = TextEditingController();
+  TextEditingController subNamesController = TextEditingController();
+  FocusNode myFocusNode = FocusNode();
   bool ifSubList = false;
   List<String> subNames = [];
 
+  String title = 'Add Task';
+
   @override
   Widget build(BuildContext context) {
-    return AlertDialog(
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.all(
-          Radius.circular(30),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+      child: AlertDialog(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(30),
+          ),
         ),
-      ),
-      title: const Text("Add Task"),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextField(
-            controller: taskNameController,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              labelText: "Task Name: ",
-            ),
-          ),
-          TextButton(
-            onPressed: () {
-              setState(() {
-                ifSubList = !ifSubList;
-              });
-            },
-            child: const Text("ADD SUBLIST +"),
-          ),
-          if (ifSubList == true)
-            TextField(
-              decoration: InputDecoration(
-                border:
-                    OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
-                labelText: "add sub task: ",
-              ),
-              onSubmitted: (value) => setState(
-                () {
-                  subNames.add(value);
+        title: Text(title),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            if (ifSubList == false || taskNameController.text == '')
+              TextField(
+                controller: taskNameController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  labelText: "Task Name: ",
+                ),
+                onSubmitted: (value) {
+                  setState(() {
+                    title = value;
+                  });
                 },
               ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  ifSubList = !ifSubList;
+                });
+              },
+              child: const Text("ADD SUBLIST +"),
             ),
-          if (ifSubList == true)
-            for (int i = 0; i < subNames.length; i++) Text(subNames[i]),
-          TextButton(
-            onPressed: () => Navigator.of(context).pop(),
-            child: const Text("DONE"),
-          ),
-        ],
+            if (ifSubList == true)
+              TextField(
+                controller: subNamesController,
+                focusNode: myFocusNode,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  labelText: "add sub task: ",
+                ),
+                onSubmitted: (value) => setState(
+                  () {
+                    subNames.add(value);
+                    subNamesController.clear();
+                    myFocusNode.requestFocus();
+                    print(taskNameController.text);
+                  },
+                ),
+              ),
+            if (ifSubList == true)
+              for (int i = 0; i < subNames.length; i++) Text(subNames[i]),
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text("DONE"),
+            ),
+          ],
+        ),
       ),
     );
   }
