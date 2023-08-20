0c6258c9c83c5f295a2e4017d15fe9ff57f819f4
Yqzi <y.qazi12@gmail.com>
2023-08-19 17:48:39 -0700

    w i P

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
-import 'dart:collection';
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:forty_days/task.dart';
 
 import 'custom_alert_dialog.dart';
 
